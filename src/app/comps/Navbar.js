d384c16978cd9276d52feab5faaeee6372e29ccc
art0881 <art89674010881@mail.ru>
2023-12-17 01:04:34 +0300

    efw

--- src/app/comps/Navbar.js
+++ src/app/comps/Navbar.js
@@ -1,15 +1,18 @@
+'use client'
 import React from 'react'
 import css from './Header.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 const Navbar = () => {
+  const pathname = usePathname()
   return (
     <header >
       <div className= {css.flexNavbar}> 
         <div><Image width={50} height={50}src='/red_logo.png'/></div>
-        <div><Link href='/' >Home</Link>
-        <Link href='/about'>About</Link>
-        <Link href='/rashid'>Rashid</Link>
+        <div><Link className={` ${pathname === '/' ? 'active' : ''}`} href='/' >Home</Link>
+        <Link className={` ${pathname === '/about' ? 'active' : ''}`} href='/about'>About</Link>
+        <Link className={` ${pathname === '/rashid' ? 'active' : ''}`} href='/rashid'>Rashid</Link>
         </div></div>
     </header>
   )
